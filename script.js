09013ba842b735f341d34d6022da26db406ae3a2
Haskic <haskica1@etf.unsa.ba>
2021-05-26 22:48:13 +0200

    Correcting mistakes, and add more functionality.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function funkcija() {
-    document.getElementById('demo').innerHTML = Date();
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementsByClassName("zaglavlje")[0].classList.add("change_color");
+  } else {
+    document.getElementsByClassName("zaglavlje")[0].classList.remove("change_color");
+  }
+  
+}
+
+function loadingPage(){
+  
+    alert("Stranica se ucitava!!");
+    document.getElementsByTagName("body")[0].style.display="block";
 }
